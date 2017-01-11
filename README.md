df30ebef6bf24e6c3ec0dd79cc61ef106e6452ab
Diego Caprioli <diego@emersonmedia.com>
2017-01-11 09:20:25 -0300

    Two simple array functions and the very first project config files.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Laravel Helpers
+
+Playing ground for some Laravel helpers.
+
+In draft mode still...
---
+++ src/Helpers/array-functions.php
@@ -0,0 +1,43 @@
+<?php
+
+
+if (!function_exists('array_has_all')) {
+
+    /**
+     * Returns true if all the $keys are present in the $array
+     *
+     * @param array $search Array to search into
+     * @param array $keys the keys that should be present in $array
+     * @return bool
+     */
+    function array_has_all(array $search, array $keys)
+    {
+        foreach ($keys as $key) {
+            if (!array_key_exists($key, $search)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+
+if (!function_exists('array_has_any')) {
+
+    /**
+     * Returns true if any of the $keys is present in the $array
+     *
+     * @param array $search
+     * @param array $keys
+     * @return bool
+     */
+    function array_has_any(array $search, array $keys) {
+        foreach ($keys as $key) {
+            if (array_key_exists($key, $search)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
