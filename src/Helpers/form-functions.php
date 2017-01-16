2e60c67fd3e2427093451ce5ebe4abe282a04b4c
Diego Caprioli <diego@emersonmedia.com>
2017-01-16 13:02:55 -0300

    Updated dependencies and added doc to the form-functions.

--- src/Helpers/form-functions.php
+++ src/Helpers/form-functions.php
@@ -1,8 +1,33 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Form helper functions
+|--------------------------------------------------------------------------
+|
+| Requires the laravelcollective Form and HTML pacakge to be installed in
+| and configured in the main project.
+| Please see: https://github.com/LaravelCollective/html
+|
+*/
+
 if (!function_exists('select_tag')) {
 
-    function select_tag($name, $list = [], $selected = null, $options = [], $listOptions = [])
+    /**
+     * Returns the output of the Form::select function, but alters the array list used as the dropdown options,
+     * using the parameters specified in $listOptions. The possible parameters are
+     * - select-option: boolean. If true, adds a new "Please select" entry at the top of the list.
+     * - select-option-message: if specified, uses this string as the "Please select" text.
+     * - select-option-key: if specified, uses this value as the option "key" used for the top "Please select" option.
+     *
+     * @param $name
+     * @param array $list
+     * @param null $selected
+     * @param array $options
+     * @param array $listOptions
+     * @return mixed
+     */
+    function select_tag($name, array $list = [], $selected = null, $options = [], $listOptions = [])
     {
         if (!isset($listOptions['select-option'])) {
             $listOptions['select-option'] = false;
