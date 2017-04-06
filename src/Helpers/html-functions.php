fef1eb1c53bd493a90d19dda6963ecb30ea19f35
Diego Caprioli <diego@emersonmedia.com>
2017-04-06 14:46:55 -0300

    Added the first version of the html functions, with a utility tag for getting human readable numbers.

--- src/Helpers/html-functions.php
+++ src/Helpers/html-functions.php
@@ -11,12 +11,25 @@
 
 if (!function_exists('human_number_tag')) {
 
-	function human_numer_tag($float, $dataToogle = 'tooltip', $dataPlacement = 'top')
+
+
+	/**
+	 * Returns the human formatted number enclosed in a Bootstrap 4 span tag 
+	 * that will have a tooltip with the complete number.
+	 * Beware that this feature requires the activation of the tooltip in your 
+	 * project!
+	 * 
+	 * @param  float $float          Number to format
+	 * @param  string $dataToogle    As defined by bootstrap 4 in it's property data-toogle
+	 * @param  string $dataPlacement As defined by bootstrap 4 in it's property data-placement
+	 * @return string
+	 */
+	function human_number_tag($float, $dataToogle = 'tooltip', $dataPlacement = 'top')
 	{
 		$formated = human_format($float);
-		$tag = '<span data-tootgle="' . $dataToogle . '" ' . 
-			'data-placement="' . $dataPlacement . '">' . 
-			$formated . '</span>';
+		return '<span data-tootgle="' . $dataToogle . '" ' . 
+			'data-placement="' . $dataPlacement . '" title="' . $float . '">' . 
+			$formated . '</span>';			
 	}
 
 }
---
+++ tests/unit/HtmlFunctionsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class HtmlFunctionsTest extends \PHPUnit\Framework\TestCase {
+
+
+    public function test_human_number_tag()
+    {        
+        $this->assertNotEmpty(human_number_tag(123456));
+    }
+    
+
+} 
