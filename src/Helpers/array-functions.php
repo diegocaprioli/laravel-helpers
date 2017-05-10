13ffc4fd2336fc277915e923585c45d4e2e13d51
Diego Caprioli <diego@emersonmedia.com>
2017-05-10 12:30:23 -0300

    Added a new array function 'array_key_has_any_value'. Refactored array tests into a single test class.

--- src/Helpers/array-functions.php
+++ src/Helpers/array-functions.php
@@ -16,7 +16,7 @@ if (!function_exists('array_has_all')) {
      * Returns true if all the $values are present in the $array
      *
      * @param array $search Array to search into
-     * @param array $values The values that should be present in $array
+     * @param array $values The values that should be present in $search
      * @return bool
      */
     function array_has_all(array $search, array $values)
@@ -40,7 +40,8 @@ if (!function_exists('array_has_any')) {
      * @param array $values
      * @return bool
      */
-    function array_has_any(array $search, array $values) {
+    function array_has_any(array $search, array $values) 
+    {
         foreach ($values as $value) {
             if (array_search($value, $search) !== false) {
                 return true;
@@ -57,7 +58,7 @@ if (!function_exists('array_key_has_all')) {
      * Returns true if all the $keys are present in the $array
      *
      * @param array $search Array to search into
-     * @param array $keys the keys that should be present in $array
+     * @param array $keys The keys that should be present in $search
      * @return bool
      */
     function array_key_has_all(array $search, array $keys)
@@ -77,14 +78,39 @@ if (!function_exists('array_key_has_any')) {
     /**
      * Returns true if any of the $keys is present in the $array
      *
-     * @param array $search
-     * @param array $keys
+     * @param array $search Array to search into
+     * @param array $keys The keys that should be present in $search
      * @return bool
      */
-    function array_key_has_any(array $search, array $keys) {
+    function array_key_has_any(array $search, array $keys) 
+    {
         foreach ($keys as $key) {
             if (array_key_exists($key, $search)) {
                 return true;
+            }
+        }
+        return false;
+    }
+
+}
+
+if (!function_exists('array_key_has_any_value')) {
+
+    /**
+     * Returs true if any of the $keys in the $search array exists and is NOT
+     * empty.
+     * 
+     * @param  array $search [description]
+     * @param  array $keys The keys that should have value in $search
+     * @return bool
+     */
+    function array_key_has_any_value(array $search, array $keys) 
+    {
+        foreach ($keys as $key) {
+            if (array_key_exists($key, $search)) {
+                if (!empty($search[$key])) {
+                    return true;
+                }
             }
         }
         return false;
---
+++ tests/unit/ArrayFunctionsTest.php
@@ -0,0 +1,71 @@
+<?php
+
+class ArrayFunctionsTest extends \PHPUnit\Framework\TestCase {
+
+    protected $search = [
+        'one'   => 'one-value',
+        'two'   => 'two-value',
+        'three' => 'three-value'
+    ];
+
+
+    public function test_array_has_all_return_true_if_all_are_present()
+    {
+        $this->assertTrue(array_has_all($this->search, ['one-value', 'three-value']));
+    }
+
+    public function test_array_has_all_return_false_if_one_is_not_present()
+    {
+        $this->assertFalse(array_has_all($this->search, ['one-value', 'four-value']));
+    }
+
+    public function test_array_has_any_return_true_if_any_are_present()
+    {
+        $this->assertTrue(array_has_any($this->search, ['one-value', 'three-value', 'four-value']));
+    }
+
+    public function test_array_has_any_return_false_if_none_are_present()
+    {
+        $this->assertFalse(array_has_any($this->search, ['six-value', 'four-value']));
+    }
+
+    public function test_array_key_has_all_return_true_if_all_are_present()
+    {
+        $this->assertTrue(array_key_has_all($this->search, ['one', 'three']));
+    }
+
+    public function test_array_key_has_all_return_false_if_one_is_not_present()
+    {
+        $this->assertFalse(array_key_has_all($this->search, ['one', 'four']));
+    }
+
+    public function test_array_key_has_any_return_true_if_any_are_present()
+    {
+        $this->assertTrue(array_key_has_any($this->search, ['one', 'three', 'four']));
+    }
+
+    public function test_array_key_has_any_return_false_if_none_are_present()
+    {
+        $this->assertFalse(array_key_has_any($this->search, ['six', 'four']));
+    }
+
+    public function test_array_key_has_any_value_return_true_if_at_least_one_key_has_value() 
+    {    	
+    	$this->assertTrue(array_key_has_any_value($this->search, ['two']));
+    }
+
+    public function test_array_key_has_any_value_return_false_if_none_isset() 
+    {    	
+    	$this->assertFalse(array_key_has_any_value($this->search, ['five', 'six']));
+    }
+
+    public function test_array_key_has_any_value_return_false_if_all_are_empty() 
+    {    	
+    	$array = [
+    		'one' => '',
+    		'two' => '',
+    	];
+    	$this->assertFalse(array_key_has_any_value($array, ['one', 'two']));
+    }
+
+} 
